3e20982e68d3511fd7df89a33d3e73f8143b9a14
Egor <egor@MacBook-Pro-Egor.local>
2017-02-11 01:05:49 +0000

    vid 32, added form editing and deleting

--- music/urls.py
+++ music/urls.py
@@ -11,4 +11,10 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'), # [0-9] = from 0 to 9, + = several digits. 712 is gonna hit
     # /music/album/add/
     url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
+    # /music/album/2/
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+
+    # /music/album/2/delete/
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
 ]
